77cd5da118573c4e628dd27b87597a62834b2d56
Vinicius Batista <mrbrazdev@gmail.com>
2023-10-12 23:19:17 -0300

    Implementing a empty function to make upload file

--- api/src/main/java/com/backend/storage/api/controller/FileController.java
+++ api/src/main/java/com/backend/storage/api/controller/FileController.java
@@ -1,13 +1,17 @@
 package com.backend.storage.api.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.backend.storage.api.entity.File;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 public class FileController {
 
     @PostMapping("/api/upload")
+    public ResponseEntity<File> uploadFile(@RequestParam("file") MultipartFile multipartFile){
+
+    }
 
     @GetMapping("/api/convert/text/{field}")
 
