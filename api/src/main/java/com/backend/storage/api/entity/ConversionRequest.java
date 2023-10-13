cfe6afe56c38b5887c73d3a1012a873de8cc4e69
Vinicius Batista <mrbrazdev@gmail.com>
2023-10-13 16:18:05 -0300

    Added the ConversionRequest class to represent conversion requests

--- api/src/main/java/com/backend/storage/api/entity/ConversionRequest.java
+++ api/src/main/java/com/backend/storage/api/entity/ConversionRequest.java
@@ -1,4 +1,43 @@
 package com.backend.storage.api.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "conversionrequest")
 public class ConversionRequest {
+
+    @Column(name = "sourcefilerequest")
+    private String sourceFileRequest;
+
+    @Column(name = "targetformat")
+    private String targetFormat;
+
+    @Column(name = "callBackUrl")
+    private String callBackUrl;
+
+    public String getSourceFileRequest() {
+        return sourceFileRequest;
+    }
+
+    public void setSourceFileRequest(String sourceFileRequest) {
+        this.sourceFileRequest = sourceFileRequest;
+    }
+
+    public String getTargetFormat() {
+        return targetFormat;
+    }
+
+    public void setTargetFormat(String targetFormat) {
+        this.targetFormat = targetFormat;
+    }
+
+    public String getCallBackUrl() {
+        return callBackUrl;
+    }
+
+    public void setCallBackUrl(String callBackUrl) {
+        this.callBackUrl = callBackUrl;
+    }
 }
