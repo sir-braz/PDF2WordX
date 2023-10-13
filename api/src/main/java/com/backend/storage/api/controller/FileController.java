0ed703b01132f04e76d914ce948da62366ad83d5
Vinicius Batista <mrbrazdev@gmail.com>
2023-10-12 23:42:31 -0300

    Add empty function for text conversion

--- api/src/main/java/com/backend/storage/api/controller/FileController.java
+++ api/src/main/java/com/backend/storage/api/controller/FileController.java
@@ -2,6 +2,7 @@ package com.backend.storage.api.controller;
 
 import com.backend.storage.api.entity.File;
 import com.backend.storage.api.utility.BackendException;
+import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,6 +13,9 @@ public class FileController {
 
 
     @GetMapping("/api/convert/text/{field}")
+    public ResponseEntity<String> convertText(@PathVariable String field){
+        
+    }
 
     @GetMapping("/api/convert/image/{filed}")
 
