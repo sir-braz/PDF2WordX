4da1469637314750a1eb318d9c522250c2754c33
Vinicius Batista <mrbrazdev@gmail.com>
2023-10-12 23:29:11 -0300

    fixed

--- api/src/main/java/com/backend/storage/api/controller/ConversionController.java
+++ api/src/main/java/com/backend/storage/api/controller/ConversionController.java
@@ -1,4 +1,23 @@
 package com.backend.storage.api.controller;
 
+import com.backend.storage.api.entity.File;
+import com.backend.storage.api.utility.BackendException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
 public class ConversionController {
+
+    @PostMapping("/api/upload")
+    public ResponseEntity<File> uploadFile(@RequestParam("file") MultipartFile multipartFile){
+        try{
+            return null;
+        }catch (BackendException ex){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        }
+    }
 }
--- api/src/main/java/com/backend/storage/api/controller/FileController.java
+++ api/src/main/java/com/backend/storage/api/controller/FileController.java
@@ -2,6 +2,7 @@ package com.backend.storage.api.controller;
 
 import com.backend.storage.api.entity.File;
 import com.backend.storage.api.utility.BackendException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -9,10 +10,6 @@ import org.springframework.web.multipart.MultipartFile;
 @RestController
 public class FileController {
 
-    @PostMapping("/api/upload")
-    public ResponseEntity<File> uploadFile(@RequestParam("file") MultipartFile multipartFile){
-
-    }
 
     @GetMapping("/api/convert/text/{field}")
 
